bc8f83a47913ecf49209bf1d280783eb8f75a425
+ <+>
2023-09-30 05:01:00 -0400

    created new functions

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 console.log(introductionWithLanguage("Aki","Ember.js"));
 
 
-function introductionWithLanguage(name, language = "Javascript"){
-return `Hi, my name is ${name} and i am learning to program in ${language}.`;
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-console.log(introductionWithLanguage("Aki",));
+console.log(introductionWithLanguageOptional("Gracie", "JavaScript"));
 
 
